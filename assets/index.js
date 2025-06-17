07ea269c0a45ceafe07853aa96703400d9b8a733
BabyDiv <inna.statsenko.work@gmail.com>
2025-06-17 20:20:46 +0500

    Add header

--- assets/index.js
+++ assets/index.js
@@ -11,6 +11,30 @@ document.querySelectorAll('.has-dropdown').forEach((item) => {
   });
 });
 
+document.querySelectorAll('.item__sub li.has-dropdown').forEach((subItem) => {
+  const itemSubSub = subItem.querySelector('.item__sub_sub');
+
+  subItem.addEventListener('mouseenter', () => {
+    itemSubSub?.classList.remove('visibility-hidden');
+  });
+
+  subItem.addEventListener('mouseleave', () => {
+    itemSubSub?.classList.add('visibility-hidden');
+  });
+});
+
+document.querySelectorAll('.item__footer.has-dropdown').forEach((item) => {
+  const itemUl = item.querySelector('.item__ul');
+
+  item.addEventListener('mouseenter', () => {
+    itemUl?.classList.remove('visibility-hidden');
+  });
+
+  item.addEventListener('mouseleave', () => {
+    itemUl?.classList.add('visibility-hidden');
+  });
+});
+
 let burger = document.getElementById('burger');
 let navList = document.querySelector('.nav__list');
 let burgerIcon = document.querySelector('.burger__icon');
@@ -25,5 +49,44 @@ burger.addEventListener('click', () => {
 
   
 console.log('index.js загружен 🎉');
+
+// let array = [
+//   {
+//     text: "Hello",
+//     url: "https://yandex.ru/search?lr=194",
+//     links: [
+//       {
+//         text: "Masha",
+//         url: "masha.com"
+//       },
+//       { 
+//         text: "Vanya",
+//         url: "Vanya.com"
+//       },
+//       {
+//         text: "Kesha",
+//         url: "InnokentiyTheGreat.com"
+//       }
+//     ]
+//   },
+//   {
+//     "text": "Whatsup",
+//     "url": "https://www.google.co.uk/"
+//   },
+//   {
+//     "text": "Goodbye",
+//     "url": "https://www.pornhub.com"
+//   }
+// ];
+
+// for (let i = 0; i < array.length; i++) {
+//   console.log(array[i].text);
+//   console.log(array[i].url)
+// }
+
+// array[0].links.forEach(link => {
+//   console.log(link.text),
+//   console.log(link.url)
+// })
 
 
